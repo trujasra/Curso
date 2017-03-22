edb4b1461a56d944822f894f218c43bb46260a75
Victor Aguilar <victoraguilar.net@gmail.com>
2017-03-21 20:28:56 -0400

    agregando jquery

---
+++ js/index.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    
+    var preloader = new $.materialPreloader({
+        position: 'top',
+        height: '5px',
+        col_1: '#159756',
+        col_2: '#da4733',
+        col_3: '#3b78e7',
+        col_4: '#fdba2c',
+        fadeIn: 200,
+        fadeOut: 200
+    });
+
+    var $button = $("#button");
+
+    // Values
+    var $firstName =  $("#firstName");
+    var $lastName =  $("#lastName");
+    var $email =  $("#email");
+    var $suggestion=  $("#suggestion");
+
+    $button.click(function(e){
+        console.log("Click!");
+
+        var suggestion = {
+            first_name: $firstName.val(),
+            last_name: $lastName.val(),
+            email: $email.val(),
+            suggestion: $suggestion.val()
+        }
+
+        preloader.on();
+
+        setTimeout(function(){
+            console.log(suggestion);
+            alert("datos recibidos...");
+            preloader.off();
+        }, 5000);
+    });
+});
